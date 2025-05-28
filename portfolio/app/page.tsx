2e268d3f9f5bad1a6042a750e08c38c065cd06c8
Matthieu <matthieufranques@gmail.com>
2025-05-28 12:00:59 +0200

    Feat : new bg

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -10,12 +10,36 @@ import TrainingTimeLine from "./Components/trainingTimeline";
 export default function Home() {
   return (
     <main className="main-custom">
-      <AboutMe />
-      <TechClound />
-      <TechTicker />
-      <Projects />
-      <CareerPage />
-      <TrainingTimeLine />
+      <div
+        className="w-full min-h-screen pt-48"
+        style={{ backgroundColor: "#f4fff8", color: "#111827" }}
+      >
+        <AboutMe />
+      </div>
+      <div className="bg-[#50808e] text-white w-full">
+        <TechClound />
+        <TechTicker />
+      </div>
+      <div
+        className="w-full"
+        style={{ backgroundColor: "#f4fff8", color: "#111827" }}
+      >
+        <Projects />
+      </div>
+
+      <div
+        className="w-full"
+        style={{ backgroundColor: "#e9b44c", color: "#111827" }}
+      >
+        <CareerPage />
+      </div>
+
+      <div
+        className="w-full"
+        style={{ backgroundColor: "#50808e", color: "white" }}
+      >
+        <TrainingTimeLine />
+      </div>
     </main>
   );
 }
